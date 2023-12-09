4d492cd3ca521e28e37726cb797c08025926f8f0
Mena Bebawy <eng.mena.lorans@gmail.com>
2023-12-09 19:37:15 +0100

    MatchServiceImplTest uses AssertJ instead of jupiter

--- lib/src/test/java/live/match/service/MatchServiceImplTest.java
+++ lib/src/test/java/live/match/service/MatchServiceImplTest.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MatchServiceImplTest {
     MatchService matchService;
@@ -27,10 +28,10 @@ class MatchServiceImplTest {
     @Test
     void givenFreshStartedMatch_thenAllFieldAreDefault() throws StartNewMatchException {
         Match match = getStartedMatchBetweenHomeAndAway();
-        assertEquals(0, match.getHomeTeamScore());
-        assertEquals(0, match.getAwayTeamScore());
-        assertEquals(0, match.getScore());
-        assertFalse(match.isFinished());
+        assertThat(match.getHomeTeamScore()).isZero();
+        assertThat(match.getAwayTeamScore()).isZero();
+        assertThat(match.getScore()).isZero();
+        assertThat(match.isFinished()).isFalse();
     }
 
     // endregion
@@ -40,43 +41,42 @@ class MatchServiceImplTest {
     void givenTeamsScoreForFinishedMatch_whenUpdateMatch_thenThrowInvalidMatchStateException() throws InvalidMatchStateException, StartNewMatchException, MatchNotFoundException {
         Match match = getStartedMatchBetweenHomeAndAway();
         matchService.finish(match.getId());
-        Exception exception = assertThrows(InvalidMatchStateException.class,
-                                           () -> matchService.update(match.getId(), 2, 0));
-        assertNotNull(exception);
+
+        assertThatThrownBy(() -> matchService.update(match.getId(), 2, 0))
+                .isInstanceOf(InvalidMatchStateException.class);
     }
 
     @Test
     void givenNotStartedMatchYet_whenUpdateMatch_thenThrowMatchNotFoundException() {
         Match match = new Match("id", System.nanoTime(), new Team("Team1"), new Team("Team2"));
-        Exception exception = assertThrows(MatchNotFoundException.class,
-                                           () -> matchService.update(match.getId(), 1, 0));
-        assertNotNull(exception);
+        assertThatThrownBy(() -> matchService.update(match.getId(), 1, 0))
+                .isInstanceOf(MatchNotFoundException.class);
     }
 
     @Test
     void givenTeamScoreLessThanZero_whenUpdateMatch_thenThrowInvalidMatchStateException() throws InvalidMatchStateException, StartNewMatchException, MatchNotFoundException {
         Match match = getStartedMatchBetweenHomeAndAway();
         matchService.update(match.getId(), 2, 0);
-        Exception exception = assertThrows(InvalidMatchStateException.class,
-                                           () -> matchService.update(match.getId(), 2, -1));
-        assertNotNull(exception);
+
+        assertThatThrownBy(() -> matchService.update(match.getId(), 2, -1))
+                .isInstanceOf(InvalidMatchStateException.class);
     }
 
     @Test
     void givenScoreLessThanCurrent_whenUpdateMatch_thenThrowInvalidMatchStateException() throws InvalidMatchStateException, StartNewMatchException, MatchNotFoundException {
         Match match = getStartedMatchBetweenHomeAndAway();
         matchService.update(match.getId(), 2, 0);
-        Exception exception = assertThrows(InvalidMatchStateException.class,
-                                           () -> matchService.update(match.getId(), 0, 1));
-        assertNotNull(exception);
+
+        assertThatThrownBy(() -> matchService.update(match.getId(), 0, 1))
+                .isInstanceOf(InvalidMatchStateException.class);
     }
 
     @Test
     void givenValidScores_whenUpdateMatch_thenNewScoresUpdated() throws InvalidMatchStateException, StartNewMatchException, MatchNotFoundException {
         Match match = getStartedMatchBetweenHomeAndAway();
         Match updatedMatch = matchService.update(match.getId(), 1, 0);
-        assertEquals(1, updatedMatch.getHomeTeamScore());
-        assertEquals(0, updatedMatch.getAwayTeamScore());
+        assertThat(updatedMatch.getHomeTeamScore()).isEqualTo(1);
+        assertThat(updatedMatch.getAwayTeamScore()).isZero();
     }
 
     // endregion
@@ -86,21 +86,22 @@ class MatchServiceImplTest {
     void givenFinishedMatchId_whenFinishMatch_thenInvalidMatchStateException() throws StartNewMatchException, InvalidMatchStateException, MatchNotFoundException {
         Match match = getStartedMatchBetweenHomeAndAway();
         matchService.finish(match.getId());
-        Exception exception = assertThrows(InvalidMatchStateException.class, () -> matchService.finish(match.getId()));
-        assertNotNull(exception);
+
+        assertThatThrownBy(() -> matchService.finish(match.getId()))
+                .isInstanceOf(InvalidMatchStateException.class);
     }
 
     @Test
     void givenNotFoundMatchId_whenFinishMatch_thenMatchNotFoundException() {
-        Exception exception = assertThrows(MatchNotFoundException.class, () -> matchService.finish("id"));
-        assertNotNull(exception);
+        assertThatThrownBy(() -> matchService.finish("id"))
+                .isInstanceOf(MatchNotFoundException.class);
     }
 
     @Test
     void givenInProgressMatchId_whenFinishMatch_thenMatchFinisher() throws StartNewMatchException, InvalidMatchStateException, MatchNotFoundException {
         Match match = getStartedMatchBetweenHomeAndAway();
         Match finishedMatch = matchService.finish(match.getId());
-        assertTrue(finishedMatch.isFinished());
+        assertThat(finishedMatch.isFinished()).isTrue();
     }
 
     private Match getStartedMatchBetweenHomeAndAway() throws StartNewMatchException {
